9452c183a75f157fbd14a602950ab6b9c559ed3e
drlight17 <37434652+drlight17@users.noreply.github.com>
2020-04-11 23:27:28 +0300

    rus and fixes

--- html/js/admin.js
+++ html/js/admin.js
@@ -1,36 +1,40 @@
 var servers = [];
-var timer, create = true;
+var timer, create = true, checked= false;
 var serverName, mac, ip, id, broadcast, username, password, usernameDisplay, level;
 
 function getServers(){
-	$.post("servers.php", {action:"get"}).done(function(data) {
-		servers = data;
-		
-		$("#serverlist").html("");
-		for(var server in servers){
-			const thisServer = servers[server];
-			var row = $("<tr><td>" + thisServer.name + "</td><td>" + thisServer.ip + "</td><td></td><td></td></tr>");
-			var col = $('<div class="btn-group"/>');
-			col.append('<a class="btn btn-primary"><i class="glyphicon glyphicon-off"></i> Wake Up</a>').children().click(function(){wake(thisServer);});
-			col.append($('<a class="btn btn-default"><i class="glyphicon glyphicon-refresh"></i> Refresh</a>').click(function(){ping(thisServer);}));
-			
-			if(level > 1){
-				
-				var btngrp = $('<div class="btn-group"><a class="btn btn-default dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></a><ul class="dropdown-menu" role="menu"></ul></div>');
-			
-				btngrp.children("ul").append($('<li><a><i class="glyphicon glyphicon-pencil"></i> Edit</a></li>').click(function(){edit(thisServer);}));
-				btngrp.children("ul").append($('<li><a><i class="glyphicon glyphicon-trash"></i> Remove</a></li>').click(function(){remove(thisServer);}));
-				
-				col.append(btngrp);
-			}
-			
-			row.children(':last').append(col);
-			$("#serverlist").append(row);
-			thisServer.statusField = $("#serverlist tr:last td:nth-child(3)");
-		}
-		
-		pingAll();
-	});
+        $.post("servers.php", {action:"get"}).done(function(data) {
+                servers = data;
+
+                $("#serverlist").html("");
+                for(var server in servers){
+                        const thisServer = servers[server];
+                        var row = $("<tr><td>" + thisServer.name + "</td><td>" + thisServer.ip + "</td><td></td><td></td></tr>");
+                        var col = $('<div class="btn-group"/>');
+//                      var addbutton = $('body > div.container > div.btn-group.pull-right > button.btn.btn-success.require-level-2');
+                        col.append('<a class="btn btn-primary"><i class="glyphicon glyphicon-off"></i> Разбудить</a>').children().click(function(){wake(thisServer);});
+                        col.append($('<a class="btn btn-default"><i class="glyphicon glyphicon-refresh"></i> Обновить</a>').click(function(){ping(thisServer);}));
+
+                        if(level > 1){
+
+                                var btngrp = $('<div class="btn-group"><a class="btn btn-default dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></a><ul class="dropdown-menu" role="menu"></ul></div>');
+        var addbutton = $('body > div.container > div.btn-group.pull-right > button.btn.btn-success.require-level-2');
+
+                                btngrp.children("ul").append($('<li><a><i class="glyphicon glyphicon-pencil"></i> Редактировать</a></li>').click(function(){edit(thisServer);}));
+                                btngrp.children("ul").append($('<li><a><i class="glyphicon glyphicon-trash"></i> Удалить</a></li>').click(function(){remove(thisServer);}));
+
+                                col.append(btngrp);
+                        } /*else {
+;                           addbutton.hide();
+*/                      }
+
+                        row.children(':last').append(col);
+                        $("#serverlist").append(row);
+                        thisServer.statusField = $("#serverlist tr:last td:nth-child(3)");
+                }
+
+                pingAll();
+        });
 }
 
 function guid() {
@@ -41,144 +45,171 @@ function guid() {
 }
 
 function addServer() {
-	serverName.val("");
-	mac.val("");
-	ip.val("");
-	broadcast.val("");
-	id = guid();
-	create = true;
-	$("#modal").modal('show');
+        serverName.val("");
+        mac.val("");
+        ip.val("");
+        broadcast.val("");
+        id = guid();
+        create = true;
+//      if (!(checked)) {
+//      $("#modal").modal('show');
+            $("#modal").modal({backdrop: 'static', keyboard: false});
+//      };
 }
 
 function edit(server) {
-	serverName.val(server.name);
-	mac.val(server.mac);
-	ip.val(server.ip);
-	broadcast.val(server.broadcast);
-	id = server.id;
-	create = false;
-	$("#modal").modal('show');
+        serverName.val(server.name);
+        mac.val(server.mac);
+        ip.val(server.ip);
+        broadcast.val(server.broadcast);
+        id = server.id;
+        create = false;
+        $("#modal").modal({backdrop: 'static', keyboard: false});
+//      $("#modal").modal('show');
 }
 
 function save() {
-	$.post("servers.php", {id:id, mac:mac.val(), ip:ip.val(),name:serverName.val(),broadcast:broadcast.val(), action: (create ? "add" : "modify")})
-		.done(function(data){
-			bootbox.alert("Server Saved");
-			getServers();
-		})
-		.fail(function(data){
-			bootbox.alert("error:" + data.status);
-		});
+        if ((mac.val()=="")||(ip.val()=="")||(serverName.val()=="")||(broadcast.val()=="")) {
+//          alert ("Все поля обязательны к заполнению!");
+            bootbox.alert("Все поля обязательны к заполнению!");
+//          checked = true;
+//          addServer();
+//          $("#modal").modal({backdrop: 'static', keyboard: false});
+            return;
+            //$("#modal").modal('show');
+        } else {
+            $.post("servers.php", {id:id, mac:mac.val(), ip:ip.val(),name:serverName.val(),broadcast:broadcast.val(), action: (create ? "add" : "modify")})
+
+                .done(function(data){
+                        bootbox.alert("Компьютер сохранен");
+                        getServers();
+                })
+                .fail(function(data){
+                        bootbox.alert("Ошибка:" + data.status);
+                });
+            $("#modal").modal('hide');
+        }
 }
 
 function remove(server) {
-	bootbox.confirm("Are you shure you want to delete \"" + server.name + '"', function(result){
-		if(result){
-			$.post("servers.php", {id:server.id, action:"remove"})
-				.done(function(data){
-					bootbox.alert("Server deleted");
-					getServers();
-				})
-				.fail(function(data){
-					bootbox.alert("error:" + data.status);
-				});
-		}
-	});
+        bootbox.confirm("Вы действительно хотите удалить компьютер \"" + server.name + '"', function(result){
+                if(result){
+                        $.post("servers.php", {id:server.id, action:"remove"})
+                                .done(function(data){
+                                        bootbox.alert("Компьютер удален");
+                                        getServers();
+                                })
+                                .fail(function(data){
+                                        bootbox.alert("Ошибка:" + data.status);
+                                });
+                }
+        });
 }
 
 function wake(server) {
-	$.post("wake.php", {mac:server.mac, broadcast: server.broadcast})
-		.done(function(data){
-			bootbox.alert("Server woken");
-			ping(server);
-		})
-		.fail(function(data){
-			bootbox.alert("error:" + data.status);
-		});
+        $.post("wake.php", {mac:server.mac, broadcast: server.broadcast})
+                .done(function(data){
+                        bootbox.alert("Попытка разбудить компьютер сделана. Попробуйте обновить статус через несколько секунд.");
+                        ping(server);
+                })
+                .fail(function(data){
+                        bootbox.alert("Ошибка:" + data.status);
+                });
 }
 
 function ping(server){
-	server.statusField.html("Loading...");
-	server.statusField.css({color:"blue"});
-	
-	$.post("ping.php", {ip:server.ip})
-		.done(function(data){
-			if(data.response == "alive"){
-				server.statusField.html("Alive");
-				server.statusField.css({color:"green"});
-			}
-			if(data.response == "dead"){
-				server.statusField.html("Dead");
-				server.statusField.css({color:"red"});
-			}
-		})
-		.fail(function(data){
-			server.statusField.html("&lt;error&gt;");
-			server.statusField.css({color:"orange"});
-		});
+        server.statusField.html("Проверка...");
+        server.statusField.css({color:"blue"});
+
+        $.post("ping.php", {ip:server.ip})
+                .done(function(data){
+                        if(data.response == "alive"){
+                                server.statusField.html("Включен");
+                                server.statusField.css({color:"limegreen"});
+                        }
+                        if(data.response == "dead"){
+                                server.statusField.html("Отключен");
+                                server.statusField.css({color:"red"});
+                        }
+                })
+                .fail(function(data){
+                        server.statusField.html("&lt;Ошибка&gt;");
+                        server.statusField.css({color:"orange"});
+                });
 }
 
 function pingAll(){
-	for(var server in servers){
-		ping(servers[server]);
-	}
+        for(var server in servers){
+                ping(servers[server]);
+        }
 }
 
 function logout(){
-	$.post("login.php", {a:"logout"})
-		.done(function(){
-			usernameDisplay.html("");
-			level = 0;
-			showLogin(true);
-		});
+        $.post("login.php", {a:"logout"})
+                .done(function(){
+                        usernameDisplay.html("");
+                        level = 0;
+                        showLogin();
+                });
 }
 
 function login(){
-	$.post("login.php", {a:"login", username:username.val(), password:password.val()})
-		.done(function(data){
-			if(data.response){
-				showLogin();
-			}
-			else {
-				showLogin(true);
-			}
-		})
-		.fail(function(data){
-			bootbox.alert("Error");
-			console.log(data);
-		});
+        $("#login-modal").modal('hide');
+        $('body').removeClass('modal-open');
+        $('.modal-backdrop').remove();
+        $.post("login.php", {a:"login", username:username.val(), password:password.val()})
+                .done(function(data){
+                        if(data.response){
+                                showLogin();
+                        }
+                        else {
+                                //alert ("Вы не ввели логин и/или пароль!")
+                                showLogin(true);
+                                getServers();
+                        }
+                })
+                .fail(function(data){
+                        bootbox.alert("Error");
+                        console.log(data);
+                });
 }
 
 function showLogin(nocheck = false){
-	if(nocheck){
-		$("#login-modal").modal('show');
-		return;
-	}
-	$.post("login.php", {a: "status"})
-		.done(function(data){
-			if (data.response == null){
-				$("#login-modal").modal('show');
-			}
-			else {
-				usernameDisplay.html(data.response.username);
-				level = data.response.level;
-				$('.require-level-2').css({display:(level >= 2)});
-				$('.require-level-1').css({display:(level >= 1)});
-				$('.require-level-3').css({display:(level >= 3)});
-				getServers();
-			}
-		});
+        if(nocheck){
+                alert ("Не введены или не верные данные авторизации!");
+                $("#login-modal").modal({backdrop: 'static', keyboard: false});
+                return;
+        }
+        $.post("login.php", {a: "status"})
+                .done(function(data){
+                        if (data.response == null){
+
+                                $("#login-modal").modal({backdrop: 'static', keyboard: false});
+                                }
+                        else {
+                                usernameDisplay.html("Вы вошли как: "+ "<strong>" + data.response.username + "</strong>");
+                                level = data.response.level;
+                                $('.require-level-2').css({display:(level >= 2)});
+                                $('.require-level-1').css({display:(level >= 1)});
+                                $('.require-level-3').css({display:(level >= 3)});
+                                if(level<=1) {addbutton.hide()} else {addbutton.show()};
+                                getServers();
+                        }
+                });
 }
 
 window.onload = function(){
-	level = 0;
-	serverName = $("#name");
-	mac = $("#mac");
-	ip = $("#ip");
-	username = $("#username");
-	broadcast = $("#broadcast");
-	usernameDisplay = $('#username-display');
-	password = $('#password');
-	
-	showLogin();
+        //level = data.response.level;
+        addbutton = $('body > div.container > div.btn-group.pull-right > button.btn.btn-success.require-level-2');
+        //if (level<=1) addbutton.hide();
+        level = 0;
+        serverName = $("#name");
+        mac = $("#mac");
+        ip = $("#ip");
+        username = $("#username");
+        broadcast = $("#broadcast");
+        usernameDisplay = $('#username-display');
+        password = $('#password');
+
+        showLogin();
 }
