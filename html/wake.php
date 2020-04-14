9e28e8c3eb6120a81627b51fda4039244cd0fb79
drlight17 <37434652+drlight17@users.noreply.github.com>
2020-04-15 01:27:57 +0300

    RC-2

--- html/wake.php
+++ html/wake.php
@@ -1,50 +1,50 @@
 <?php
-	include_once 'auth.php';
-	include_once 'db.php';
-	
-	$auth = new Auth();
-	
-	if(!$auth->hasLevel()) {
-		http_response_code(401);
-	}
-	else if(isset($_POST['mac'], $_POST['broadcast'])) {
-		$broadcast =  $_POST['broadcast'];
-		$mac_array = split(':', $_POST['mac']);
+        include_once 'auth.php';
+        include_once 'db.php';
 
-		$hwaddr = '';
+        $auth = new Auth();
 
-		foreach($mac_array AS $octet) {
-			$hwaddr .= chr(hexdec($octet));
-		}
+        if(!$auth->hasLevel(0)) {
+                http_response_code(401);
+        }
+        else if(isset($_POST['mac'], $_POST['broadcast'])) {
+                $broadcast =  $_POST['broadcast'];
+                $mac_array = preg_split('/:/', $_POST['mac']);
 
-		// Create Magic Packet
+                $hwaddr = '';
 
-		$packet = '';
-		for ($i = 1; $i <= 6; $i++) {
-			$packet .= chr(255);
-		}
+                foreach($mac_array AS $octet) {
+                        $hwaddr .= chr(hexdec($octet));
+                }
 
-		for ($i = 1; $i <= 16; $i++) {
-			$packet .= $hwaddr;
-		}
+                // Create Magic Packet
 
-		$sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
-		if($sock) {
-			$options = socket_set_option($sock, 1, 6, true);
+                $packet = '';
+                for ($i = 1; $i <= 6; $i++) {
+                        $packet .= chr(255);
+                }
 
-			if ($options >=0) {    
-				$e = socket_sendto($sock, $packet, strlen($packet), 0, $broadcast, 7);
-				socket_close($sock);
-			}    
-		}
-	}
-	else {
-		http_response_code(400);
-	}
-	
-	$response = array(
-		"status" => http_response_code()
-	);
-	header('Content-Type: application/json');
-	echo json_encode($response);
+                for ($i = 1; $i <= 16; $i++) {
+                        $packet .= $hwaddr;
+                }
+
+                $sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+                if($sock) {
+                        $options = socket_set_option($sock, 1, 6, true);
+
+                        if ($options >=0) {
+                                $e = socket_sendto($sock, $packet, strlen($packet), 0, $broadcast, 7);
+                                socket_close($sock);
+                        }
+                }
+        }
+        else {
+                http_response_code(400);
+        }
+
+        $response = array(
+                "status" => http_response_code()
+        );
+        header('Content-Type: application/json');
+        echo json_encode($response);
 ?>
